17441abc33c520e69e7695c0d01b1b4a4f8a1bbe
aditya-balachander <aditya.balachander@salesforce.com>
2023-10-04 15:41:47 +0530

    added functionality

---
+++ cumulusci/salesforce_api/tooling_retrieve.py
@@ -0,0 +1,198 @@
+import json
+from concurrent.futures import ThreadPoolExecutor
+from typing import Dict, List, Union
+
+from salesforce_bulk.util import IteratorBytesIO
+from simple_salesforce.exceptions import SalesforceGeneralError
+
+from cumulusci.tasks.salesforce.BaseSalesforceApiTask import BaseSalesforceApiTask
+
+SETUP_ENTITY_TYPES = {
+    "ApexClass": {"columns": ["Name", "NamespacePrefix"], "table_name": "ApexClass"},
+    "ApexPage": {"columns": ["Name", "NamespacePrefix"], "table_name": "ApexPage"},
+    "CustomPermission": {
+        "columns": ["DeveloperName", "NamespacePrefix"],
+        "table_name": "CustomPermission",
+    },
+    "ExternalDataSource": {
+        "columns": ["DeveloperName", "NamespacePrefix"],
+        "table_name": "ExternalDataSource",
+    },
+}
+SETUP_ENTITY_QUERY_NAME = "setupEntityAccess"
+
+SOBJECT_TYPE = "CustomObject"
+SOBJECT_QUERY_NAME = "sObject"
+
+CUSTOM_TAB_TYPE = "CustomTab"
+CUSTOM_TAB_QUERY_NAME = "customTab"
+
+
+class ToolingApiTask(BaseSalesforceApiTask):
+    def _init_task(self):
+        super(ToolingApiTask, self)._init_task()
+
+    def _run_queries_in_parallel(self, queries: Dict[str, str]) -> Dict[str, list]:
+        num_threads = 4
+        results_dict = {}
+
+        with ThreadPoolExecutor(max_workers=num_threads) as executor:
+            futures = {
+                query_name: executor.submit(self._run_query, query)
+                for query_name, query in queries.items()
+            }
+
+        for query_name, future in futures.items():
+            try:
+                query_result = future.result()
+                results_dict[query_name] = query_result["records"]
+            except Exception as e:
+                raise Exception(f"Error executing query '{query_name}': {type(e)}: {e}")
+            else:
+                queries.pop(query_name, None)
+
+        return results_dict
+
+    def _run_query(self, query):
+        try:
+            result = self.sf.query(query)
+        except SalesforceGeneralError:
+            result = self._run_bulk_query(query)
+        return result
+
+    def _run_bulk_query(self, query):
+        table_name = self._extract_table_name_from_query(query)
+        job = self.bulk.create_query_job(table_name, contentType="JSON")
+
+        batch = self.bulk.query(job, query)
+        self.bulk.wait_for_batch(job, batch)
+        self.bulk.close_job(job)
+        results = self.bulk.get_all_results_for_query_batch(batch)
+        for result in results:
+            result = json.load(IteratorBytesIO(result))
+            return {"records": result}
+
+    def _extract_table_name_from_query(self, query):
+        from_index = query.upper().find("FROM")
+        if from_index != -1:
+            query = query[from_index + len("FROM") :].strip()
+            table_name = query.split()[0]
+            return table_name
+        else:
+            raise ValueError("Invalid query format. 'FROM' clause not found.")
+
+    def _build_query(
+        self,
+        columns: List[str],
+        table_name: str,
+        where: Dict[str, Union[str, List[str]]] = None,
+    ) -> str:
+        if not columns:
+            raise ValueError("Columns list cannot be empty")
+        if not table_name:
+            raise ValueError("Table name cannot be empty")
+
+        select_clause = ", ".join(columns)
+
+        where_clause = ""
+        if where:
+            where_list = []
+            for key, value in where.items():
+                if isinstance(value, list):
+                    value = ", ".join([f"'{item}'" for item in value])
+                    condition = f"{key} IN ({value})"
+                else:
+                    condition = f"{key} = '{value}'"
+                where_list.append(condition)
+            where_clause = "WHERE " + " AND ".join(where_list)
+
+        query = f"SELECT {select_clause} FROM {table_name} {where_clause}"
+        return query
+
+    # Retrieve all the permissionable entitites for a set of profiles
+    def _retrieve_permissionable_entities(self, profiles: List[str]):
+        queries = {}
+
+        # Setup Entity Access query
+        setupEntityAccess_query = self._build_query(
+            ["SetupEntityId", "SetupEntityType"],
+            "SetupEntityAccess",
+            {"Parent.Profile.Name": profiles},
+        )
+        queries[SETUP_ENTITY_QUERY_NAME] = setupEntityAccess_query
+
+        # sObject query
+        sObject_query = self._build_query(
+            ["SObjectType"], "ObjectPermissions", {"Parent.Profile.Name": profiles}
+        )
+        queries[SOBJECT_QUERY_NAME] = sObject_query
+
+        # Custom Tab query
+        customTab_query = self._build_query(
+            ["Name"], "PermissionSetTabSetting", {"Parent.Profile.Name": profiles}
+        )
+        queries[CUSTOM_TAB_QUERY_NAME] = customTab_query
+
+        # Run all queries
+        result = self._run_queries_in_parallel(queries)
+
+        # Process the results
+        permissionable_entities = {}
+        permissionable_entities.update(
+            self._process_setupEntityAccess_results(result[SETUP_ENTITY_QUERY_NAME])
+        )
+        permissionable_entities.update(
+            self._process_sObject_results(result[SOBJECT_QUERY_NAME])
+        )
+        permissionable_entities.update(
+            self._process_customTab_results(result[CUSTOM_TAB_QUERY_NAME])
+        )
+
+        return permissionable_entities
+
+    def _process_setupEntityAccess_results(self, result_list: List[dict]):
+        setupEntityAccess_dict = {
+            entity_type: [] for entity_type in SETUP_ENTITY_TYPES.keys()
+        }
+
+        for data in result_list:
+            entity_type = data["SetupEntityType"]
+            entity_id = data["SetupEntityId"]
+            if entity_type in SETUP_ENTITY_TYPES.keys():
+                setupEntityAccess_dict[entity_type].append(entity_id)
+
+        queries = {}
+        for entity_type, query_values in SETUP_ENTITY_TYPES.items():
+            if query_values and len(setupEntityAccess_dict[entity_type]) != 0:
+                queries[entity_type] = self._build_query(
+                    query_values["columns"],
+                    query_values["table_name"],
+                    {"Id": setupEntityAccess_dict[entity_type]},
+                )
+
+        result = self._run_queries_in_parallel(queries)
+
+        extracted_values = {}
+        for entity_type, data in SETUP_ENTITY_TYPES.items():
+            if entity_type in result and data:
+                extracted_values[entity_type] = []
+                for item in result[entity_type]:
+                    if (
+                        "NamespacePrefix" in item
+                        and item["NamespacePrefix"] is not None
+                    ):
+                        extracted_values[entity_type].append(
+                            f'{item["NamespacePrefix"]}__{item[data["columns"][0]]}'
+                        )
+                    else:
+                        extracted_values[entity_type].append(item[data["columns"][0]])
+
+        return extracted_values
+
+    def _process_sObject_results(self, result_list: List[dict]):
+        sObject_list = [data["SobjectType"] for data in result_list]
+        return {SOBJECT_TYPE: sObject_list}
+
+    def _process_customTab_results(self, result_list: List[dict]):
+        customTab_list = [data["Name"] for data in result_list]
+        return {CUSTOM_TAB_TYPE: customTab_list}
--- cumulusci/tasks/salesforce/retrieve_profile.py
+++ cumulusci/tasks/salesforce/retrieve_profile.py
@@ -1,7 +1,10 @@
 from cumulusci.salesforce_api.metadata import ApiRetrieveUnpackaged
+from cumulusci.salesforce_api.tooling_retrieve import ToolingApiTask
 from cumulusci.tasks.salesforce.BaseSalesforceMetadataApiTask import (
     BaseSalesforceMetadataApiTask,
 )
+
+EXTRACT_DIR = "./unpackaged"
 
 
 class RetrieveProfile(BaseSalesforceMetadataApiTask):
@@ -19,21 +22,32 @@ class RetrieveProfile(BaseSalesforceMetadataApiTask):
     def _run_task(self):
 
         self.name = self.options["name"]
+        self.tooling_task = ToolingApiTask(
+            project_config=self.project_config,
+            task_config=self.task_config,
+            org_config=self.org_config,
+        )
+        self.tooling_task._init_task()
+        permissionable_entities = self.tooling_task._retrieve_permissionable_entities(
+            [
+                self.name,
+            ]
+        )
+        entities_to_be_retrieved = {
+            **permissionable_entities,
+            **{
+                "Profile": [
+                    self.name,
+                ]
+            },
+        }
 
-        self.package_xml = f"""
-        <?xml version="1.0" encoding="UTF-8"?>
-        <Package xmlns="http://soap.sforce.com/2006/04/metadata">
-            <types>
-                <members>{self.name}</members>
-                <name>Profile</name>
-            </types>
-            <version>54.0</version>
-        </Package>
-        """
-
+        self.package_xml = self._create_package_xml(entities_to_be_retrieved)
         api = self._get_api()
         zip_result = api()
-        zip_result.extractall("./unpackaged")
+        for file_info in zip_result.infolist():
+            if file_info.filename.startswith("profiles/"):
+                zip_result.extract(file_info, EXTRACT_DIR)
         self.logger.info(f"Profile '{self.name}' unzipped into folder 'unpackaged'")
 
     def _get_api(self):
@@ -42,3 +56,19 @@ class RetrieveProfile(BaseSalesforceMetadataApiTask):
             api_version=self.org_config.latest_api_version,
             package_xml=self.package_xml,
         )
+
+    def _create_package_xml(self, input_dict: dict, api_version="58.0"):
+        package_xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
+        package_xml += '<Package xmlns="http://soap.sforce.com/2006/04/metadata">\n'
+
+        for name, members in input_dict.items():
+            package_xml += "    <types>\n"
+            for member in members:
+                package_xml += f"        <members>{member}</members>\n"
+            package_xml += f"        <name>{name}</name>\n"
+            package_xml += "    </types>\n"
+
+        package_xml += f"    <version>{api_version}</version>\n"
+        package_xml += "</Package>\n"
+
+        return package_xml
